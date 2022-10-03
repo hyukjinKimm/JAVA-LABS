cf4b7ca77e7ddc0a4d1a6fc0acafbe5d69806887
hyukjinKimm <hyukjin.kim.dev@gmail.com>
2022-10-04 08:09:56 +0900

    변수와 자료형 (2)

---
+++ Chapter2/src/binary/ByteVariable.java
@@ -0,0 +1,10 @@
+package binary;
+
+public class ByteVariable {
+
+	public static void main(String[] args) {
+		byte n1 = -128;
+		byte n2 = 127;
+	}
+
+}
---
+++ Chapter2/src/binary/CharacterEx.java
@@ -0,0 +1,16 @@
+package binary;
+
+public class CharacterEx {
+
+	public static void main(String[] args) {
+		char ch = 'A';
+		System.out.println((int)ch);
+		
+		ch = 128;
+		System.out.println(ch);		
+		
+		int ch2 = 67;
+		System.out.println((char)ch2);
+	}
+
+}
