f196e371fc29d250c6265309f0fb5fd254cd9f73
hyukjinKimm <hyukjin.kim.dev@gmail.com>
2022-10-04 07:51:13 +0900

    변수와 자료형 (1)

---
+++ Chapter2/src/binary/BinaryTest.java
@@ -0,0 +1,17 @@
+package binary;
+
+public class BinaryTest {
+
+	public static void main(String[] args) {
+		int bnum = 0B1010;
+		int onum = 012 ;
+		int hnum = 0XA;
+		
+		System.out.println(bnum);
+		System.out.println(onum);
+		System.out.println(hnum);
+	
+				
+	}
+
+}
