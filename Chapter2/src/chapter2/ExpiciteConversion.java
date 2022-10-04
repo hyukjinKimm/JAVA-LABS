4b14ea4661e90cdad2a381f76902efd75eaef37a
hyukjinKimm <hyukjin.kim.dev@gmail.com>
2022-10-05 00:08:51 +0900

    변수와 자료형 (3)

---
+++ Chapter2/src/chapter2/ExpiciteConversion.java
@@ -0,0 +1,15 @@
+package chapter2;
+
+public class ExpiciteConversion {
+
+	public static void main(String[] args) {
+		int num = 1;
+		byte bnum = (byte)num;
+		System.out.println(bnum);
+		
+		double dnum = 3.14;
+	    int nnum = (int)dnum;
+		System.out.println(nnum);
+	}
+
+}
