1d3560a8a0552a52b04687bfe1b7ffe95952efcd
hyukjinkimm <hyukjin.kim.dev@gmail.com>
2022-10-11 20:00:46 +0900

    객체배열 사용하기

---
+++ Chapter7/src/array/ObjectCopy.java
@@ -0,0 +1,36 @@
+package array;
+
+public class ObjectCopy {
+
+	public static void main(String[] args) {
+		Book[] bookArray1 = new Book[3];
+		Book[] bookArray2 = new Book[3];
+		
+		bookArray1[0] = new Book("태산", "저자1");
+		bookArray1[1] = new Book("태산", "저자2");
+		bookArray1[2] = new Book("태산", "저자3");
+		
+		System.arraycopy(bookArray1, 0, bookArray2, 0, 3);
+		
+		for ( int i = 0 ; i < bookArray2.length; i ++) {
+			bookArray2[i].showBookInfo();
+			
+		}
+		bookArray1[0].setAuthor("김수지");
+		bookArray1[0].setBookName("차차차");
+		
+		for ( int i = 0 ; i < bookArray2.length; i ++) {
+			bookArray2[i].showBookInfo();
+			
+		}
+		
+		
+	  String[] strarr = {"ajava", "dads", "ads"};
+	  for(String s: strarr) {
+		  System.out.println(s);
+	  }
+
+		
+	}
+
+}
