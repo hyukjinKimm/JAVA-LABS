f9e75f024374e5c7b0134690c3560c30a44877c9
hyukjinkimm <hyukjin.kim.dev@gmail.com>
2022-10-15 21:53:47 +0900

    다형성 활용과 다운캐스팅

--- Chapter8/src/inheritance/AnimalTest.java
+++ Chapter8/src/inheritance/AnimalTest.java
@@ -10,11 +10,18 @@ class Human extends Animal {
 	public void move() {
 		System.out.println("사람이 두발로 걷습니다.");
 	}
+	public void readBook() {
+		System.out.println("사람이 책을 읽습니다.");		
+	}
 }
 
 class Tiger extends Animal {
 	public void move() {
 		System.out.println("호랑이가 네발로 뜁니다.");
+	}
+	
+	public void hunt() {
+		System.out.println("호랑이가  사냥합니다.");		
 	}
 }
 public class AnimalTest {
@@ -29,6 +36,17 @@ public class AnimalTest {
 	}
 	public void moveAnimal(Animal animal) {
 		animal.move();
+		
+		if (animal instanceof Human) {
+			Human human = (Human)animal;
+			human.readBook();
+		}
+		else if (animal instanceof Tiger) {
+			Tiger tiger = (Tiger)animal;
+			tiger.hunt();
+		} else {
+			System.out.println("지원되지 않는 기능 입니다.");
+		}
 	}
 
 }
